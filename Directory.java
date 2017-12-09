2a5652e9fbd15f40ca7bd387233196745c22afa4
bstone22 <bstone22@uw.edu>
2017-12-08 22:55:52 -0800

    some directory fucntions

--- Directory.java
+++ Directory.java
@@ -4,6 +4,8 @@ public class Directory {
    // Directory entries
    private int fsize[];        // each element stores a different file size.
    private char fnames[][];    // each element stores a different file name.
+   private int sizeDirectory;  // size of the directory
+   private int maxNumber;
 
    public Directory( int maxInumber ) { // directory constructor
       fsizes = new int[maxInumber];     // maxInumber = max files
@@ -13,6 +15,8 @@ public class Directory {
       String root = "/";                // entry(inode) 0 is "/"
       fsize[0] = root.length( );        // fsize[0] is the size of "/".
       root.getChars( 0, fsizes[0], fnames[0], 0 ); // fnames[0] includes "/"
+      sizeDirectory = maxInumber;
+      maxNumber = maxInumber;
    }
 
    public void bytes2directory( byte data[] ) {
@@ -32,12 +36,36 @@ public class Directory {
       // allocates a new inode number for this filename
    }
 
-   public boolean ifree( short iNumber ) {
-      // deallocates this inumber (inode number)
-      // the corresponding file will be deleted.
+
+   //boolean ifree - short Inumber
+   //
+   //this function takes in an iNumber as a parameter and checks if its greater than 
+   //the max number of inumbers and if the number is not less than 0.  If the parameter
+   //is already free in the array, it returns false as well.  If the iNumber passes
+   //these two test, then the fsize is set to 0 and fnames set to null.
+   public boolean ifree( short iNumber ) 
+   {
+      if(iNumber > maxNumber || iNumber < 0) {return false;} //inumber is greater than the max and less than 0, invalid
+
+      if(fsize[iNumber == 0]) {return false;}                //inumber is already free
+
+      fsizes[iNumber] = 0;
+      fnames[iNumber] = null;         //****NOT SURE HERE*****//Should we also delete the name contents?
+      return true;
+
    }
 
-   public short namei( String filename ) {
-      // returns the inumber corresponding to this filename
+   //short namei - String filename
+   //
+   //this function takes in a filename and checks fname to see if
+   //the name is in the array.  if it is found it returns the index
+   //of the that found filename. 
+   public short namei( String filename ) 
+   {
+      for(int i = 0; i < sizeDirectory; i++)
+      {
+         if(filename.equals(new String fnames[i])) { return (short)i;}
+      }
+      return -1;
    }
 }
