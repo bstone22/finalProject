74fd07451fa412132cdb53ddac69c30a610f1609
bstone22 <bstone22@uw.edu>
2017-12-10 14:51:32 -0800

    Directory Done

--- Directory.java
+++ Directory.java
@@ -1,68 +1,118 @@
+/************************************************************************
+*                                                                       *
+* Author: Brent Stone & Prarin Behdarvandian                            *
+* Date:   12/07/17                                                      *
+* CSS 430                                                               *
+* Program 5 - File System                                               *
+*                                                                       *
+************************************************************************/
 public class Directory {
-   private static int maxChars = 30; // max characters of each file name
+   private final int maxChars = 30; // max characters of each file name
+   private final int BYTES_ALLOC = 64;
+   private final int MAX_BYTES = 60;
+   private final int CHAR_MAX = 30;
+   private final int BLOCK_SPACE = 4; 
 
    // Directory entries
    private int fsize[];        // each element stores a different file size.
    private char fnames[][];    // each element stores a different file name.
    private int sizeDirectory;  // size of the directory
-   private int maxNumber;
+   private int maxNumber;      //
+
 
    public Directory( int maxInumber ) { // directory constructor
-      fsizes = new int[maxInumber];     // maxInumber = max files
+      fsize = new int[maxInumber];     // maxInumber = max files
       for ( int i = 0; i < maxInumber; i++ ) 
          fsize[i] = 0;                 // all file size initialized to 0
       fnames = new char[maxInumber][maxChars];
       String root = "/";                // entry(inode) 0 is "/"
       fsize[0] = root.length( );        // fsize[0] is the size of "/".
-      root.getChars( 0, fsizes[0], fnames[0], 0 ); // fnames[0] includes "/"
+      root.getChars( 0, fsize[0], fnames[0], 0 ); // fnames[0] includes "/"
       sizeDirectory = maxInumber;
       maxNumber = maxInumber;
    }
 
-   public void bytes2directory( byte data[] ) 
-   {
-      int offset = 0;
-      for(int i = 0; i < fsizes.length; i++, offest += 4)
-      {
-         fsizes[i] = Syslib.bytes2int(data, offset);
-      }
-
-      for(int i = 0; i < fnames.length; i++, offset += maxChars * 2) 
-      {
-         String fname = new String(data, offset, maxChars *2);
-         fname.getChars(0, fsizes[i], fnames[i], 0);
-      }
-
-   }
-
-   public byte[] directory2bytes( ) {
-      // converts and return Directory information into a plain byte array
-      // this byte array will be written back to disk
-      // note: only meaningfull directory information should be converted
-      // into bytes.
-   }
-
-   public short ialloc( String filename ) {
-      // filename is the one of a file to be created.
-      // allocates a new inode number for this filename
-   }
-
-
-   //boolean ifree - short Inumber
+   //byte directory2bytes
    //
-   //this function takes in an iNumber as a parameter and checks if its greater than 
-   //the max number of inumbers and if the number is not less than 0.  If the parameter
-   //is already free in the array, it returns false as well.  If the iNumber passes
-   //these two test, then the fsize is set to 0 and fnames set to null.
+   //This function is responsible for coverting bytes 2 directory
+   //overall function was inspired by slides that has the bytes to
+   //directory function.  
+   public void bytes2directory(byte data[]) 
+   {
+      int spacing = 0;
+      for (int i = 0; i < sizeDirectory; i++) {
+         
+         fsize[i] = SysLib.bytes2int(data, spacing);   //bytes to int int the fsize array   
+         spacing += BLOCK_SPACE;                      //increment by block space = 4
+      }
+      
+      for (int i = 0; i < sizeDirectory; i++) 
+      {
+         (new String(data, spacing, MAX_BYTES)).getChars(0, fsize[i], fnames[i], 0);   //calls new string to create and 
+         spacing += MAX_BYTES;                                                         //chars from  
+         //increase by max byes = 60;  
+      }         
+   }
+
+   //byte directory2bytes
+   //
+   //This function is responsible for coverting directory information
+   //int an byte array that will be written to the disk. 
+   public byte[] directory2bytes( ) 
+   {
+      int spacing = 0;                                                  //block spacking
+      byte[] directoryToBytes = new byte[sizeDirectory * BYTES_ALLOC];  //byte array
+
+      for(int i = 0; i < sizeDirectory; i++)
+      {
+         SysLib.int2bytes(fsize[i],directoryToBytes,spacing);        //calls int to bytes with parametrts
+         spacing += 4;                                      //increase block spacing by 4
+      }
+
+      for(int i = 0; i < sizeDirectory; i++)
+      {
+         byte[] temp = (new String(fnames[i],0,fsize[i])).getBytes();      //gets bytes for string using both f
+                                                                           //fnames and fsize         
+         System.arraycopy(temp, 0, directoryToBytes,spacing,temp.length);        //copy
+         spacing += 60;                                                    //increase by max byes 
+      }
+
+      return directoryToBytes;
+   }
+
+   // short ialloc - String filename
+   
+   // the function is responsible fpr allcoating a new file using the next
+   // available iNumber within the fsize.   
+   public short ialloc( String filename ) 
+   {
+      for (int i = 0; i < sizeDirectory; i++)
+      {
+         if(fsize[i] == 0)                   //if not in sue
+         {
+            fsize[i] = Math.min(filename.length(), maxChars); //take the smaller size
+            filename.getChars(0,fsize[i],fnames[i],0);        //call cetChars on the file
+            return (short) i;                                 //return index
+         }
+      }
+      return (short)-1;
+   }
+
+   // boolean ifree - short Inumber
+   
+   // this function takes in an iNumber as a parameter and checks if its greater than 
+   // the max number of inumbers and if the number is not less than 0.  If the parameter
+   // is already free in the array, it returns false as well.  If the iNumber passes
+   // these two test, then the fsize is set to 0 and fnames set to null.
    public boolean ifree( short iNumber ) 
    {
-      if(iNumber > maxNumber || iNumber < 0) {return false;} //inumber is greater than the max and less than 0, invalid
-
-      if(fsize[iNumber == 0]) {return false;}                //inumber is already free
-
-      fsizes[iNumber] = 0;
-      fnames[iNumber] = null;         //****NOT SURE HERE*****//Should we also delete the name contents?
-      return true;
+      if(iNumber < CHAR_MAX && fsize[iNumber] > 0) 
+         {
+            fsize[iNumber] = 0;
+            return true;
+         } //inumber is greater than the max and less than 0, invalid
+      
+      return false;
 
    }
 
@@ -73,9 +123,14 @@ public class Directory {
    //of the that found filename. 
    public short namei( String filename ) 
    {
-      for(int i = 0; i < sizeDirectory; i++)
+      for(short i = 0; i < fsize.length; i++)
       {
-         if(filename.equals(new String fnames[i])) { return (short)i;}
+        if(fsize[i] == filename.length())
+        {
+            String check = new String(fnames[i],0,fsize[i]);
+
+            if(check.equals(filename)) {return i;}
+        }
       }
       return -1;
    }
